5995b0e9c06b098b443bc20a85fc166a45618064
toyoaki_shinada <toshinad@gmail.com>
2018-05-31 17:07:03 +0900

    test

--- infra/codedeploy/master/afterInstall.sh
+++ infra/codedeploy/master/afterInstall.sh
@@ -3,8 +3,11 @@
 ENVIRONMENT=$DEPLOYMENT_GROUP_NAME
 
 BASE=/opt/codepipeline-test
+DEPLOY=$BASE-$ENVIRONMENT
+CURRENT=$BASE-$ENVIRONMENT.`date '+%Y%m%d%H%M%S'`
 
 cd $BASE/..
-mv codepipeline-test codepipeline-test-$ENVIRONMENT.`date '+%Y%m%d%H%M%S'`
+mv $BASE $CURRENT
+ln -nfs $CURRENT $DEPLOY
 
 echo "$ENVIRONMENT afterInstall" >> /tmp/result.txt
