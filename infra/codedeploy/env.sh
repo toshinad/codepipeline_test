50d90d3305ef9b83d737f7217ccb88ff49f817e5
toyoaki_shinada <toshinad@gmail.com>
2018-06-01 03:49:42 +0900

    test

--- infra/codedeploy/env.sh
+++ infra/codedeploy/env.sh
@@ -2,4 +2,7 @@
 
 export ENVIRONMENT=$DEPLOYMENT_GROUP_NAME
 export BASE=/opt/codepipeline-test
-export DEPLOY=$BASE-$ENVIRONMENT
+
+export DEPLOY=$BASE-$ENVIRONMENT-`date
+export CURRENT=$BASE-ENVIRONMENT-`date '+%Y%m%d%H%M%S'`
+`
