bf98ead3e9da48d83c54434a1e5cd2141b35236a
toyoaki_shinada <toshinad@gmail.com>
2018-06-01 03:12:59 +0900

    test

--- infra/codedeploy/beforeInstall.sh
+++ infra/codedeploy/beforeInstall.sh
@@ -3,7 +3,10 @@
 set -e
 
 echo `pwd`
-. /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/infra/codedeploy/env.sh
+if [ -e /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/infra/codedeploy/env.sh ]
+then
+  . /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/infra/codedeploy/env.sh
+fi
 
 if [ -e $DEPLOY ]
 then
