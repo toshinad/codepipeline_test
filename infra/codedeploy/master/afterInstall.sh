1cb228ec6bdd02f2bfbb8325deb641fdd53e450d
toyoaki_shinada <toshinad@gmail.com>
2018-05-31 17:08:18 +0900

    test

--- infra/codedeploy/master/afterInstall.sh
+++ infra/codedeploy/master/afterInstall.sh
@@ -9,5 +9,6 @@ CURRENT=$BASE-$ENVIRONMENT.`date '+%Y%m%d%H%M%S'`
 cd $BASE/..
 mv $BASE $CURRENT
 ln -nfs $CURRENT $DEPLOY
+ln -nfs $CURRENT $BASE
 
 echo "$ENVIRONMENT afterInstall" >> /tmp/result.txt
