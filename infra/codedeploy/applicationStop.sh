c8597ced4821693653ff01c92cffa415da4e0060
toyoaki_shinada <toshinad@gmail.com>
2018-05-31 23:12:08 +0900

    test

--- infra/codedeploy/applicationStop.sh
+++ infra/codedeploy/applicationStop.sh
@@ -5,10 +5,13 @@ set -e
 echo `pwd`
 . /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/infra/codedeploy/env.sh
 
-cd $DEPLOY
-
-if [ -e ./infra/codedeploy/$ENVIRONMENT/applicationStart.sh ]
+if [ -d $DEPLOY ]
 then
-  chmod +x ./infra/codedeploy/$ENVIRONMENT/applicationStop.sh
-  ./infra/codedeploy/$ENVIRONMENT/applicationStop.sh
+  cd $DEPLOY
+
+  if [ -e ./infra/codedeploy/$ENVIRONMENT/applicationStart.sh ]
+  then
+    chmod +x ./infra/codedeploy/$ENVIRONMENT/applicationStop.sh
+    ./infra/codedeploy/$ENVIRONMENT/applicationStop.sh
+  fi
 fi
--- infra/codedeploy/beforeInstall.sh
+++ infra/codedeploy/beforeInstall.sh
@@ -5,10 +5,13 @@ set -e
 echo `pwd`
 . /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/infra/codedeploy/env.sh
 
-cd $DEPLOY
-
-if [ -e ./infra/codedeploy/$ENVIRONMENT/beforeInstall.sh ]
+if [ -e $DEPLOY ]
 then
-  chmod +x ./infra/codedeploy/$ENVIRONMENT/beforeInstall.sh
-  ./infra/codedeploy/$ENVIRONMENT/beforeInstall.sh
+  cd $DEPLOY
+
+  if [ -e ./infra/codedeploy/$ENVIRONMENT/beforeInstall.sh ]
+  then
+    chmod +x ./infra/codedeploy/$ENVIRONMENT/beforeInstall.sh
+    ./infra/codedeploy/$ENVIRONMENT/beforeInstall.sh
+  fi
 fi
