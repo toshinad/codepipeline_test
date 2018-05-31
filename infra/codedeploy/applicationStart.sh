117e7b176c7b013707d4c7f256718e7c1e8059e4
toyoaki_shinada <toshinad@gmail.com>
2018-06-01 04:50:40 +0900

    test

--- infra/codedeploy/applicationStart.sh
+++ infra/codedeploy/applicationStart.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-ENVIRONMENT=$DEPLOYMENT_GROUP_NAME
-
 export ENVIRONMENT=$DEPLOYMENT_GROUP_NAME
-export BASE=/opt/app/instagram-admin
+export BASE=/opt/codepipeline-test
 export DEPLOY=$BASE-$ENVIRONMENT
 export CURRENT=$BASE-$ENVIRONMENT-`date '+%Y%m%d%H%M%S'`
 
